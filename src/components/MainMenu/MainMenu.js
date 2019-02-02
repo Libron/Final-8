19ed05a8f15800198bcd8a273587f32915717119
Urmat Bekboev <bek.urmat@gmail.com>
2019-02-02 13:03:07 +0600

    Moved to corresponding directory

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import {NavLink as RouterNavLink} from "react-router-dom";
+import {NavLink as RRNavLink} from "react-router-dom";
 import {Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink} from "reactstrap";
 
 const MainMenu = () => {
     return (
-        <Navbar color="dark" dark expand="md">
+        <Navbar color="dark" dark expand="md" style={{"marginBottom": "50px"}}>
             <NavbarBrand href="/">Quotes Central</NavbarBrand>
             <NavbarToggler />
             <Collapse isOpen navbar>
                 <Nav className="ml-auto" navbar>
                     <NavItem>
-                        <NavLink tag={RouterNavLink} to="/" exact>Quotes</NavLink>
+                        <NavLink tag={RRNavLink} to="/" exact>Quotes</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink tag={RouterNavLink} to="/add-quote" >Submit new quote</NavLink>
+                        <NavLink tag={RRNavLink} to="/add-quote" >Submit new quote</NavLink>
                     </NavItem>
                 </Nav>
             </Collapse>
